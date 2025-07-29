e9ede7d790d82774c36dc2e1e835e7b6fe60d86e
Marlliton Souza <marlliton.souza1@gmail.com>
2025-07-29 18:46:13 -0300

    feat: inicia exemplo de arrays

---
+++ cmd/array-example/main.go
@@ -0,0 +1,12 @@
+package arrayexample
+
+func main() {
+	eggBox := [12]string{
+		"ovo 1", "ovo 2",
+		"ovo 3", "ovo 4",
+		"ovo 5", "ovo 6",
+		"ovo 7", "ovo 8",
+		"ovo 9", "ovo 10",
+		"ovo 11", "ovo 12",
+	}
+}
---
+++ pkg/array/array.go
@@ -0,0 +1,4 @@
+package array
+
+// NOTE: This package is created to maintain the project's structure,
+// even though arrays are a built-in data type.
